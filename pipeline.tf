1c0d966b0a4d36d93e2714198d6919bb1faa3553
kingziieke <josh.zalman@triumphtech.com>
2022-10-20 09:44:22 -0400

    Update pipeline.tf

--- pipeline.tf
+++ pipeline.tf
@@ -1,5 +1,5 @@
-resource "aws_codebuild_project" "jz-tf-cicd-plan" {
-  name         = "jz-tf-cicd-plan"
+resource "aws_codebuild_project" "jz-tf-plan" {
+  name         = "jz-tf-plan"
   description  = "Plan stage for terraform"
   service_role = aws_iam_role.jz-tf-codebuild-role.arn
 
