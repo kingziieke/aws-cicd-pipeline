e2571af1d9a7587798498af6c97fd9f1fd10e7d4
kingziieke <josh.zalman@triumphtech.com>
2022-10-20 14:01:59 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "jz-codepipeline-artifacts" {
   bucket = "jz-pipeline-artifacts"
+}
+
+resource "aws_s3_bucket_acl" "jz-codepipeline-artifacts-acl" {
+  bucket = aws_s3_bucket.jz-codepipeline-artifacts.id
   acl    = "private"
 }
