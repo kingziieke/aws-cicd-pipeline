0ddb505ced15ee0ed322fe3c3c310645c92b331e
kingziieke <josh.zalman@triumphtech.com>
2022-10-20 09:58:55 -0400

    Update pipeline.tf

--- pipeline.tf
+++ pipeline.tf
@@ -82,7 +82,7 @@ resource "aws_codepipeline" "jz-cicd_pipeline" {
       owner           = "AWS"
       input_artifacts = ["tf-code"]
       configuration = {
-        ProjectName = "jz-tf-cicd-plan"
+        ProjectName = "jz-tf-plan"
       }
     }
   }
